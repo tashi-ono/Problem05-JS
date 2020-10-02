faa227ae70a4a80117b8041474b4622d2ea727c0
Natasha Sana <nfsana@gmail.com>
2020-10-02 11:17:46 -0700

    push to github

--- EuclidsAlgorithm.js
+++ EuclidsAlgorithm.js
@@ -1,9 +1,13 @@
 class EuclidsAlgorithm {
-    gcd(a, b){
-        let divisor = -1;
-
-        return divisor;
+  gcd(a, b) {
+    if (a === 0) {
+      return b;
+    } else if (b === 0) {
+      return a;
+    } else {
+      return this.gcd(b, a % b);
     }
+  }
 }
 
 module.exports = EuclidsAlgorithm;
